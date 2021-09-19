fixed scaling/normalization

main.py
@@ -3,7 +3,8 @@ import glob
 import os
 import scipy.io
 
-from data_preprocessing.data_distribution import aggregate_data, create_uniform_distribution, z_score_normalization
+from data_preprocessing.data_distribution import aggregate_data, create_uniform_distribution, z_score_normalization, \
+    max_absolute_scaling, min_max_scaling
 from definitions import DATASET_PATH
 from classes import Dataset
 from data_preprocessing.date_freq_convertion import convert_mat_date_to_python_date
@@ -38,7 +39,7 @@ def init(selected_cue_set=0):
     dataset.time_after_first_window = convert_mat_date_to_python_date(cue_set['time_after_first_window'])
     dataset.time_after_last_window = convert_mat_date_to_python_date(cue_set['time_after_last_window'])
     dataset.time_stop_device1 = convert_mat_date_to_python_date(cue_set['time_stop_device1'])
-    dataset.data_device1 = pd.DataFrame(cue_set['data_device1'])
+    dataset.data_device1 = pd.DataFrame(cue_set['data_device1'][15:])
     dataset.time_axis_all_device1 = pd.DataFrame(cue_set['time_axis_all_device1'])
 
     return dataset
@@ -49,15 +50,18 @@ if __name__ == '__main__':
 
     trigger_table = trigger_time_table(data.TriggerPoint, data.time_start_device1)
 
-    labelled_data = aggregate_data(data.data_device1, 200, trigger_table, sample_rate=data.sample_rate)
+    # normalization / scaling techniques
+    # data.data_device1 = z_score_normalization(data.data_device1)
+    # data.data_device1 = max_absolute_scaling(data.data_device1)
+    # data.data_device1 = min_max_scaling(data.data_device1)
+
+    labelled_data = aggregate_data(data.data_device1, 100, trigger_table, sample_rate=data.sample_rate)
     uniform_data = create_uniform_distribution(labelled_data)
-    # z_scored_data = z_score_normalization(uniform_data)
 
     train_data, test_data = train_test_split_data(uniform_data, split_per=20)
 
     score = knn_classifier_all_channels(train_data, test_data)
 
-    print(pd.DataFrame(score))
 
 
 