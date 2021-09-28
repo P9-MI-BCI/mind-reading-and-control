eb13e3b76f8249d56e0c9e2dbb7e106052b3c55f
Cryppen <34452759+Cryppen@users.noreply.github.com>
2021-09-28 14:07:04 +0200

    svm, lgbm added, improved code

---
+++ data_training/LGBM/lgbm_prediction.py
@@ -0,0 +1,24 @@
+import lightgbm as lgb
+
+from data_training.scikit_classifiers import scikit_classifier
+
+
+def lightGBM_classifier_params(SEED: int):
+    return {
+        "objective": "binary",
+        "boosting_type": "dart",
+        "max_depth": 10,
+        "learning_rate": 0.05,
+        "bagging_seed": SEED,
+        "random_state": SEED,
+        "n_jobs": -1,
+    }
+
+
+def lgbm_classifier(train_data, test_data, channels=None):
+
+    model = lgb.LGBMClassifier(**lightGBM_classifier_params(SEED=1337))
+
+    result = scikit_classifier(model, train_data, test_data, channels)
+
+    return result
