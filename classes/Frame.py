738cede3b804b6344e7654b2f93580268055981a
Cryppen <34452759+Cryppen@users.noreply.github.com>
2021-10-01 11:36:56 +0200

    added filtered function in class Frame

--- classes/Frame.py
+++ classes/Frame.py
@@ -1,6 +1,14 @@
+import pandas as pd
+
+
 class Frame:
 
-    def __int__(self, label=0, data=0, timestamp=0):
+    def __int__(self, label: int = 0, data: pd.DataFrame = 0, timestamp: pd.Series = 0,
+                filtered_data: pd.DataFrame = 0):
         self.label = label
         self.data = data
         self.timestamp = timestamp
+        self.filtered_data = filtered_data
+
+    def filter(self, filter_in):
+        self.filtered_data = filter_in(self.data)
