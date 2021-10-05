f7574d479042460743ef776062c26b287d65ae08
Zirke <defrenefre@hotmail.com>
2021-10-05 14:48:42 +0200

    Refractor of filters

--- data_preprocessing/filters.py
+++ data_preprocessing/filters.py
@@ -3,39 +3,77 @@ from scipy.signal import butter, sosfilt, lfilter, filtfilt
 from utility.logger import get_logger
 
 
-def butter_helper(order, cutoff, btype, freq):
-    nyq = 0.5 * freq
-    sos = None
-    if btype == 'highpass' and len(cutoff) == 1:
-        normal_cutoff = cutoff[0] / nyq
-        b, a = butter(order, normal_cutoff, analog=False, btype=btype, output='ba')
-    elif btype == 'bandpass' and len(cutoff) == 2:
-        normal_low = cutoff[0] / nyq
-        normal_high = cutoff[1] / nyq
-        sos = butter(N=order, Wn=(normal_low, normal_high), btype=btype, fs=freq, output='sos')
-    elif btype == 'lowpass' and len(cutoff) == 1:
-        normal_cutoff = cutoff[0] / nyq
-        sos = butter(order, normal_cutoff, analog=False, btype=btype, output='sos')
-    else:
-        get_logger().error('Unsupported filter type, select \'highpass\' with len(cutoff) == 1, or \'bandpass\' with len(cutoff) == 2')
-    return b, a
-
-
 def butter_sos_highpass(order, cutoff, freq):
     nyq = 0.5 * freq
     normal_cutoff = cutoff[0] / nyq
-    return butter(order, normal_cutoff, analog=False, btype='highpass', output='sos')
+    return butter(N=order, Wn=normal_cutoff, analog=False, btype='highpass', output='sos')
 
 
-def butter_filter(data: pd.DataFrame, order: int = 5, cutoff=None, btype: str = 'highpass', freq: int = 1200):
-    if cutoff is None:
-        cutoff = [80]
+def butter_highpass(order, cutoff, freq):
+    nyq = 0.5 * freq
+    normal_cutoff = cutoff[0] / nyq
+    return butter(order, normal_cutoff, analog=False, btype='highpass', output='ba')
 
-    if btype == 'sos_highpass':
+
+def butter_sos_bandpass(order, cutoff, freq):
+    nyq = 0.5 * freq
+    normal_low = cutoff[0] / nyq
+    normal_high = cutoff[1] / nyq
+    return butter(N=order, Wn=[normal_low, normal_high], analog=False, btype='bandpass', output='sos')
+
+
+def butter_bandpass(order, cutoff, freq):
+    nyq = 0.5 * freq
+    normal_low = cutoff[0] / nyq
+    normal_high = cutoff[1] / nyq
+    return butter(N=order, Wn=[normal_low, normal_high], analog=False, btype='bandpass', output='ba')
+
+
+def butter_sos_lowpass(order, cutoff, freq):
+    nyq = 0.5 * freq
+    normal_cutoff = cutoff[0] / nyq
+    return butter(N=order, Wn=normal_cutoff, analog=False, btype='lowpass', output='sos')
+
+
+def butter_lowpass(order, cutoff, freq):
+    nyq = 0.5 * freq
+    normal_cutoff = cutoff[0] / nyq
+    return butter(N=order, Wn=normal_cutoff, analog=False, btype='lowpass', output='ba')
+
+
+def butter_filter(data: pd.DataFrame, order: int = 5, cutoff=80, btype: str = 'highpass', freq: int = 1200):
+    b, a, sos = None, None, None
+
+    if (not isinstance(cutoff, list) and not isinstance(cutoff, tuple)):
+        temp = []
+        temp.append(cutoff)
+        cutoff = temp
+
+    if btype == 'sos_highpass' and len(cutoff) == 1:
         sos = butter_sos_highpass(order, cutoff, freq)
         return sosfilt(sos, data)
-    else:
-        b, a = butter_helper(order, cutoff, btype, freq)
+
+    elif btype == 'highpass' and len(cutoff) == 1:
+        b, a = butter_highpass(order, cutoff, freq)
         return filtfilt(b, a, data)
+
+    elif btype == 'sos_bandpass' and len(cutoff) == 2:
+        sos = butter_sos_bandpass(order, cutoff, freq)
+        return sosfilt(sos, data)
+
+    elif btype == 'bandpass' and len(cutoff) == 2:
+        b, a = butter_bandpass(order, cutoff, freq)
+        return filtfilt(b, a, data)
+
+    elif btype == 'sos_lowpass' and len(cutoff) == 1:
+        sos = butter_sos_lowpass(order, cutoff, freq)
+        return sosfilt(sos, data)
+
+    elif btype == 'lowpass' and len(cutoff) == 1:
+        b, a = butter_lowpass(order, cutoff, freq)
+        return filtfilt(b, a, data)
+    else:
+
+        get_logger().error('Error in filter type or len(cutoff), check params')
 
 
