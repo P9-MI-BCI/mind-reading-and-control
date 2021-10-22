4c9a6aa3dd8a902ce55299a44dd75c97823267f0
Cryppen <34452759+Cryppen@users.noreply.github.com>
2021-10-22 11:23:44 +0200

    Update main.py
    merged main manually

--- main.py
+++ main.py
@@ -77,58 +77,105 @@ def init(selected_cue_set: int = 0):
 
 def main():
     dataset = init(selected_cue_set=config['id'])
+    
+    if script_params['offline_mode']:
+        
+        # Shift Data to remove startup
+        dataset = shift_data(freq=80000, dataset=dataset)
 
-    # Shift Data to remove startup
-    dataset = shift_data(freq=80000, dataset=dataset)
+        # Create table containing information when trigger points were shown/removed
+        trigger_table = trigger_time_table(dataset.TriggerPoint, dataset.time_start_device1)
 
-    # Create table containing information when trigger points were shown/removed
-    trigger_table = trigger_time_table(dataset.TriggerPoint, dataset.time_start_device1)
+        if script_params['run_mrcp_detection']:
+            # Perform MRCP Detection and update trigger_table with EMG timestamps
+            windows, trigger_table = mrcp_detection(data=dataset, tp_table=trigger_table, config=config)
 
-    if script_params['run_mrcp_detection']:
-        # Perform MRCP Detection and update trigger_table with EMG timestamps
-        windows, trigger_table = mrcp_detection(data=dataset, tp_table=trigger_table, config=config)
+            # Plotting a specific EEG channel's filtered data and showing the cut windows and their labels
+            visualize_windows(data=dataset, windows=windows, channel=4)
 
-        # Plotting a specific EEG channel's filtered data and showing the cut windows and their labels
-        visualize_windows(data=dataset, windows=windows, channel=4)
+            # Plot all filtered channels (0-9 and 12) together with the raw data
+            dataset.plot()
 
-        # Plot all filtered channels (0-9 and 12) together with the raw data
-        dataset.plot()
+            # Remove poor quality samples based on heuristic, score and blink detection
+            prune_poor_quality_samples(windows, trigger_table, config, remove=5, method=remove_worst_windows)
+            remove_windows_with_blink(dataset=dataset, windows=windows)
 
-        # Remove poor quality samples based on heuristic, score and blink detection
-        prune_poor_quality_samples(windows, trigger_table, config, remove=5, method=remove_worst_windows)
-        remove_windows_with_blink(dataset=dataset, windows=windows)
+            # Create and plot the average windows
+            avg_windows = average_channel(windows)
+            plot_average_channels(avg_windows, save_fig=False, overwrite=True)
 
-        # Create and plot the average windows
-        avg_windows = average_channel(windows)
-        plot_average_channels(avg_windows, save_fig=False, overwrite=True)
+            # Plots all individual windows together with EMG[start, peak, end] and Execution cue interval
+            for window in windows:
+                window.plot()
 
-        # Plots all individual windows together with EMG[start, peak, end] and Execution cue interval
-        for window in windows:
-            window.plot()
+            # Create distribution for training and dividing into train and test set
+            uniform_data = create_uniform_distribution(windows)
+            train_data, test_data = train_test_split_data(uniform_data, split_per=20)
 
-        # Create distribution for training and dividing into train and test set
-        uniform_data = create_uniform_distribution(windows)
-        train_data, test_data = train_test_split_data(uniform_data, split_per=20)
+            save_train_test_split(train_data, test_data, dir_name='EEG')
 
-        save_train_test_split(train_data, test_data, dir_name='EEG')
+        if script_params['run_classification']:
 
-    if script_params['run_classification']:
+            train_data, test_data = load_train_test_split(dir_name='EEG')
 
-        train_data, test_data = load_train_test_split(dir_name='EEG')
+            feature = 'features'
+            knn_score = knn_classifier(train_data, test_data, features=feature)
+            svm_score = svm_classifier(train_data, test_data, features=feature)
+            lda_score = lda_classifier(train_data, test_data, features=feature)
 
-        feature = 'features'
-        knn_score = knn_classifier(train_data, test_data, features=feature)
-        svm_score = svm_classifier(train_data, test_data, features=feature)
-        lda_score = lda_classifier(train_data, test_data, features=feature)
+            results = {
+                'KNN_results': knn_score,
+                'SVM_results': svm_score,
+                'LDA_results': lda_score
+            }
 
-        results = {
-            'KNN_results': knn_score,
-            'SVM_results': svm_score,
-            'LDA_results': lda_score
-        }
+            # Writes the test and train window plots + classifier score tables to pdf file
+            save_results_to_pdf(train_data, test_data, results, file_name='result_overview.pdf')
 
-        # Writes the test and train window plots + classifier score tables to pdf file
-        save_results_to_pdf(train_data, test_data, results, file_name='result_overview.pdf')
+    if script_params['online_mode']:
+        dataset = shift_data(freq=80000, dataset=dataset)
+
+        # Create table containing information when trigger points were shown/removed
+        trigger_table = trigger_time_table(dataset.TriggerPoint, dataset.time_start_device1)
+
+        if script_params['run_mrcp_detection']:
+            windows, trigger_table = mrcp_detection_for_online_use(data=dataset, tp_table=trigger_table, config=config)
+
+            prune_poor_quality_samples(windows, trigger_table, config, remove=10, method=remove_worst_windows)
+
+            uniform_data = create_uniform_distribution(windows)
+            train_data, test_data = train_test_split_data(uniform_data, split_per=20)
+
+            save_train_test_split(train_data, test_data, dir_name='online_EEG')
+
+        if script_params['run_classification']:
+            train_data, test_data = load_train_test_split(dir_name='online_EEG')
+
+            feature = 'raw'
+            knn_score = knn_classifier(train_data, test_data, features=feature)
+            svm_score = svm_classifier(train_data, test_data, features=feature)
+            lda_score = lda_classifier(train_data, test_data, features=feature)
+
+            results = {
+                'KNN_results': knn_score,
+                'SVM_results': svm_score,
+                'LDA_results': lda_score
+            }
+
+            # Writes the test and train window plots + classifier score tables to pdf file
+            save_results_to_pdf(train_data, test_data, results, file_name='result_overview.pdf')
+
+        if script_params['run_online_emulation']:
+            models = load_scikit_classifiers('knn')
+            index = load_index_list()
+            pair_indexes = pair_index_list(index)
+
+            get_logger().info('Starting Online Predictions.')
+            windows_on, predictions = emulate_online(dataset, config, models, features='raw')
+            get_logger().info('Finished Online Predictions.')
+
+            score = evaluate_online_predictions(windows_on, predictions, pair_indexes, channels=[3, 4, 5])
+            print(score)
 
 
 if __name__ == '__main__':
