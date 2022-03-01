updated training and added normalization. added temporary code to handle the online dataset until a real fix has been made.

data_preprocessing/data_distribution.py
@@ -6,6 +6,8 @@ from statistics import mean, stdev, median
 from classes.Window import Window
 from classes import Dataset
 import numpy as np
+from sklearn.preprocessing import StandardScaler
+
 
 
 # Finds the start of TriggerPoints and converts it to frequency and takes the window_size (in seconds) and cuts each
@@ -292,9 +294,50 @@ def data_preparation(datasets, config):
             if cluster[0]-config.window_padding*dataset.sample_rate < 0:
                 continue
             X.append(dataset.data[config.EEG_CHANNELS].iloc[
-                     cluster[0]-config.window_padding*dataset.sample_rate:
-                     cluster[0]+config.window_padding*dataset.sample_rate].to_numpy())
+                     cluster[0]-int(config.window_padding*dataset.sample_rate):
+                     cluster[0]+int(config.window_padding*dataset.sample_rate)].to_numpy())
             Y.append(dataset.label)
+    shuffler = np.random.permutation(len(X))
+    X = np.array(X)[shuffler]
+    Y = np.array(Y)[shuffler]
+
+    return X, Y
+
+
+def normalization(X):
+    scaler = StandardScaler()
+
+    flat_x = np.concatenate((X), axis=0)
+    scaler.fit(flat_x)
+    transformed_x = []
+    for x in X:
+        transformed_x.append(scaler.transform(x))
+
+    return np.array(transformed_x), scaler
+
+
+# This is a temporary method for testing, does not handle the labeling properly!
+def online_data_labeling(dataset, config, scaler):
+    X = []
+    Y = []
+    label_determiner = 0
+
+    for cluster in dataset.onsets_index:
+        if cluster[0] - config.window_padding * dataset.sample_rate < 0:
+            continue
+        X.append(
+            scaler.transform(
+                dataset.data[config.EEG_CHANNELS].iloc[
+                 cluster[0] - int(config.window_padding * dataset.sample_rate):
+                 cluster[0] + int(config.window_padding * dataset.sample_rate)].to_numpy()
+            )
+        )
+        if label_determiner % 2 == 0:
+            Y.append(0)
+        else:
+            Y.append(1)
+        label_determiner += 1
+
     shuffler = np.random.permutation(len(X))
     X = np.array(X)[shuffler]
     Y = np.array(Y)[shuffler]

data_preprocessing/emg_processing.py
@@ -6,8 +6,7 @@ from data_preprocessing.filters import butter_filter
 import matplotlib.pyplot as plt
 from scipy.stats import iqr
 
-
-def onset_detection(dataset: Dataset, config) -> [[int]]:
+def onset_detection(dataset: Dataset, config, is_online=False) -> [[int]]:
     # Filter EMG Data with specified butterworth filter params from config
     filtered_data = pd.DataFrame()
 
@@ -27,7 +26,7 @@ def onset_detection(dataset: Dataset, config) -> [[int]]:
     emg_rectified = np.abs(filtered_data[config.EMG_CHANNEL]) > threshold
     emg_onsets = emg_rectified[emg_rectified == True].index.values.tolist()
     # Group onsets based on time
-    emg_clusters = emg_clustering(emg_data=np.abs(filtered_data[config.EMG_CHANNEL]), onsets=emg_onsets)
+    emg_clusters = emg_clustering(emg_data=np.abs(filtered_data[config.EMG_CHANNEL]), onsets=emg_onsets, is_online=is_online)
 
     plot_arr = []
     for cluster in emg_clusters:
@@ -48,7 +47,7 @@ def onset_detection(dataset: Dataset, config) -> [[int]]:
     return filtered_data[config.EMG_CHANNEL]
 
 
-def emg_clustering(emg_data, onsets: [int], distance=None) -> [[int]]:
+def emg_clustering(emg_data, onsets: [int], distance=None, is_online=False) -> [[int]]:
     all_peaks = []
     if distance is None:
         distance = 100
@@ -84,7 +83,10 @@ def emg_clustering(emg_data, onsets: [int], distance=None) -> [[int]]:
                 index = onset
         all_peaks.append([onset_cluster[0], index, onset_cluster[-1]])
 
-    return remove_outliers_by_peak_activity(all_peaks, emg_data)
+    if is_online:
+        return all_peaks
+    else:
+        return remove_outliers_by_peak_activity(all_peaks, emg_data)
 
 
 # Compare all peaks and remove outliers below Q1

data_training/EEGModels/training.py
@@ -4,118 +4,96 @@ Prepare data format for using the EEG models and set up dispatch functions for t
 import numpy as np
 from utility.logger import get_logger
 from data_training.EEGModels.EEG_Models import EEGNet, DeepConvNet, ShallowConvNet
-from tensorflow.keras.callbacks import ModelCheckpoint
+from keras.callbacks import ModelCheckpoint
+from sklearn.model_selection import StratifiedKFold
+from tqdm.keras import TqdmCallback
+from keras.callbacks import EarlyStopping
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+import pandas as pd
+import seaborn as sn
 
 kernels = 1
 
 
-def EEGModels_training_hub(X, Y):
-    run_EEGNet(X, Y)
-    run_DeepConvNet(X, Y)
-    run_ShallowConvNet(X, Y)
+def EEGModels_training_hub(X, Y, online_X, online_Y):
+    eeg_net = get_EEGNet(X)
+    deep_conv_net = get_DeepConvNet(X)
+    shallow_conv_net = get_ShallowConvNet(X)
+
+    stratified_kfold_cv(X, Y, eeg_net, online_X, online_Y)
+    stratified_kfold_cv(X, Y, deep_conv_net, online_X, online_Y)
+    stratified_kfold_cv(X, Y, shallow_conv_net, online_X, online_Y)
 
 
-"""
-Given shuffled data and labels split the data int
-70% training data
-15% validation data
-15% test data
-while retaining same label distribution.
-"""
+# Creates a stratified-k-fold cross validation object that splits the data and shuffles it.
+def stratified_kfold_cv(X, Y, model, online_X, online_Y):
+    skf = StratifiedKFold(n_splits=5, shuffle=True)
+    # The initial weights are saved in order to reset the model between k-fold cv iterations
+    model.save_weights('initial_weights.h5')
+    for train_index, val_index in skf.split(X, Y):
+        model.load_weights('initial_weights.h5')
+        # Reshape data into (Num_samples, Num_channels, Num_data_points, kernel=1)
+        X_reshaped = X[train_index].reshape((X[train_index].shape[0], X[0].shape[1], X[0].shape[0], kernels))
+        X_val_reshaped = X[val_index].reshape((X[val_index].shape[0], X[0].shape[1], X[0].shape[0], kernels))
+        history = model.fit(X_reshaped, Y[train_index], batch_size=8, epochs=300,
+                            verbose=0, validation_data=(X_val_reshaped, Y[val_index]),
+                            callbacks=[TqdmCallback(verbose=0),
+                                       EarlyStopping(monitor='val_loss', patience=30)])
+
+    X_reshaped = X.reshape((X.shape[0], X[0].shape[1], X[0].shape[0], kernels))
+    X_online_reshaped = online_X.reshape((online_X.shape[0], online_X[0].shape[1], online_X[0].shape[0], kernels))
+
+    get_logger().info(f'Cross Validation Finished -- Training using entire dataset')
+    history = model.fit(X_reshaped, Y, batch_size=8, epochs=300,verbose=0,
+                        validation_data=(X_online_reshaped, online_Y),
+                        callbacks=[TqdmCallback(verbose=0),
+                        EarlyStopping(monitor='val_loss', patience=50)])
+
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.show()
+
+    Y_hat = model.predict(X_online_reshaped)
+    Y_hat = Y_hat.reshape((len(Y_hat),))
+    plot_confusion_matrix(online_Y, Y_hat)
 
 
-def train_val_test_split(X, Y):
-    try:
-        X_train, Y_train = [], []
-        X_val, Y_val = [], []
-        X_test, Y_test = [], []
-
-        for x, y in zip(X, Y):
-            if len(X_train) < len(X) * 0.7:
-                X_train.append(x)
-                Y_train.append(y)
-            elif len(X_val) < len(X) * 0.15:
-                X_val.append(x)
-                Y_val.append(y)
-            else:
-                X_test.append(x)
-                Y_test.append(y)
-
-        return np.array(X_train), np.array(Y_train), np.array(X_val), np.array(Y_val), np.array(X_test), np.array(
-            Y_test)
-    except AssertionError:
-        get_logger().error('The dataset does not contain an even distribution. Unable to split the dataset evenly')
-
-
-def run_EEGNet(X, Y):
-    X_train, Y_train, X_val, Y_val, X_test, Y_test = train_val_test_split(X, Y)
-
-    # Reshape data into (Num_samples, Num_channels, Num_data_points, kernel=1)
-    X_train = X_train.reshape((X_train.shape[0], X_train[0].shape[1], X_train[0].shape[0], kernels))
-    X_val = X_val.reshape((X_val.shape[0], X_val[0].shape[1], X_val[0].shape[0], kernels))
-    X_test = X_test.reshape((X_test.shape[0], X_test[0].shape[1], X_test[0].shape[0], kernels))
-
-    model = EEGNet(nb_classes=1, Chans=X_train[0].shape[0], Samples=X_train[0].shape[1], dropoutRate=0.5, kernLength=32,
+def get_EEGNet(X):
+    model = EEGNet(nb_classes=1, Chans=X[0].shape[1], Samples=X[0].shape[0], dropoutRate=0.5, kernLength=32,
                    F1=8, D=2, F2=16, dropoutType='Dropout')
 
     model.compile(loss='binary_crossentropy', optimizer='adam',
                   metrics=['accuracy'])
-    numParams = model.count_params()
 
-    checkpointer = ModelCheckpoint(filepath='/tmp/checkpoint.h5', verbose=1,
-                                   save_best_only=True)
-    model.fit(X_train, Y_train, batch_size=16, epochs=300,
-              verbose=2, validation_data=(X_val, Y_val),
-              callbacks=[checkpointer])
-
-    print(model.predict(X_test))
-    print(Y_test)
+    return model
 
 
-def run_DeepConvNet(X, Y):
-    X_train, Y_train, X_val, Y_val, X_test, Y_test = train_val_test_split(X, Y)
-
-    # Reshape data into (Num_samples, Num_channels, Num_data_points, kernel=1)
-    X_train = X_train.reshape((X_train.shape[0], X_train[0].shape[1], X_train[0].shape[0], kernels))
-    X_val = X_val.reshape((X_val.shape[0], X_val[0].shape[1], X_val[0].shape[0], kernels))
-    X_test = X_test.reshape((X_test.shape[0], X_test[0].shape[1], X_test[0].shape[0], kernels))
-
-    model = DeepConvNet(nb_classes=1, Chans=X_train[0].shape[0], Samples=X_train[0].shape[1], dropoutRate=0.5)
+def get_DeepConvNet(X):
+    model = DeepConvNet(nb_classes=1, Chans=X[0].shape[1], Samples=X[0].shape[0], dropoutRate=0.5)
 
     model.compile(loss='binary_crossentropy', optimizer='adam',
                   metrics=['accuracy'])
-    numParams = model.count_params()
 
-    checkpointer = ModelCheckpoint(filepath='/tmp/checkpoint.h5', verbose=1,
-                                   save_best_only=True)
-    model.fit(X_train, Y_train, batch_size=16, epochs=300,
-              verbose=2, validation_data=(X_val, Y_val),
-              callbacks=[checkpointer])
+    return model
 
-    print(model.predict(X_test))
-    print(Y_test)
-
-
-def run_ShallowConvNet(X, Y):
-    X_train, Y_train, X_val, Y_val, X_test, Y_test = train_val_test_split(X, Y)
-
-    # Reshape data into (Num_samples, Num_channels, Num_data_points, kernel=1)
-    X_train = X_train.reshape((X_train.shape[0], X_train[0].shape[1], X_train[0].shape[0], kernels))
-    X_val = X_val.reshape((X_val.shape[0], X_val[0].shape[1], X_val[0].shape[0], kernels))
-    X_test = X_test.reshape((X_test.shape[0], X_test[0].shape[1], X_test[0].shape[0], kernels))
-
-    model = ShallowConvNet(nb_classes=1, Chans=X_train[0].shape[0], Samples=X_train[0].shape[1], dropoutRate=0.5)
+def get_ShallowConvNet(X):
+    model = ShallowConvNet(nb_classes=1, Chans=X[0].shape[1], Samples=X[0].shape[0], dropoutRate=0.5)
 
     model.compile(loss='binary_crossentropy', optimizer='adam',
                   metrics=['accuracy'])
-    numParams = model.count_params()
 
-    checkpointer = ModelCheckpoint(filepath='/tmp/checkpoint.h5', verbose=1,
-                                   save_best_only=True)
-    model.fit(X_train, Y_train, batch_size=16, epochs=300,
-              verbose=2, validation_data=(X_val, Y_val),
-              callbacks=[checkpointer])
+    return model
 
-    print(model.predict(X_test))
-    print(Y_test)
 
+def plot_confusion_matrix(Y, Y_hat):
+    Y_hat = [round(y_hat) for y_hat in Y_hat]
+    labels = ['close', 'open']
+    conf_matrix = confusion_matrix(Y, Y_hat)
+
+    df_cm = pd.DataFrame(conf_matrix)
+
+    sn.set(font_scale=1.4)  # for label size
+    sn.heatmap(df_cm, annot=True, annot_kws={"size": 16})  # font size
+
+    plt.show()