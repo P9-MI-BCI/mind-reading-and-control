46078f59cbf453354f763831dd0bdbcbfd0eec4c
TheKaergaard <31920988+TheKaergaard@users.noreply.github.com>
2022-04-06 14:09:48 +0200

    pyrqa implementation initial wavelet transformation implementation

---
+++ data_preprocessing/wavelet_transformation.py
@@ -0,0 +1,79 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import pywt
+
+from classes.Dataset import Dataset
+
+
+def multi_dataset_wavelet_transformation(training_data, config, wavelet_type: str, plot=False):
+    wavelet_features = []
+    for dataset in training_data:
+        wavelet_features.append(wavelet_transformation(dataset, config, wavelet_type, plot))
+
+    return wavelet_features
+
+
+def wavelet_transformation(dataset: Dataset, config, wavelet_type: str, plot=False):
+    sampling_rate = config.SAMPLE_RATE
+
+    wavelet_coeffs = []
+    for channel in dataset.filtered_data:
+        totalscal = 64  # scale
+        wavename = 'morl'
+        fc = pywt.central_frequency(wavename)  # central frequency
+        cparam = 2 * fc * totalscal
+        scales = cparam / np.arange(1, totalscal + 1)
+        sampling_period = 1.0 / sampling_rate
+        time = len(dataset.filtered_data[channel]) / sampling_rate
+        t = np.arange(0, time, 1.0 / sampling_rate)
+
+        # Discrete wavelet transform
+        if wavelet_type == 'discrete':
+            cA, cD = discrete_wavelet_transform(dataset.filtered_data[channel], level=0)
+            coeffs = discrete_wavelet_transform(dataset.filtered_data[channel], level=5)
+
+            if plot:
+                fig, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex=True)
+                ax1.plot(dataset.filtered_data[channel])
+                ax2.plot(cA, '-g')
+                ax3.plot(cD, '-r')
+                fig.suptitle(f'{dataset.filename} - {channel}')
+                plt.show()
+
+        # Continuous wavelet transform
+        if wavelet_type == 'continuous':
+            cwtmatr, freqs = continuous_wavelet_transform(dataset.filtered_data[channel], scales, wavename,
+                                                          sampling_period)
+
+            wavelet_coeffs.append(cwtmatr)
+
+            if plot:
+                fig = plt.figure(1)
+                plt.contourf(t, freqs, abs(cwtmatr))
+                plt.ylabel(u"freq(Hz)")
+                plt.xlabel(u"time(s)")
+                plt.colorbar()
+                # plt.ylim(0, 60)
+                plt.show()
+
+            return wavelet_coeffs
+
+
+def discrete_wavelet_transform(channel_data, level: int):
+    max_level = pywt.dwt_max_level(len(channel_data), 'db4')
+
+
+    if level > 0:
+        # Returns: [cA_n, cD_n, cD_n-1, …, cD2, cD1] : list
+        coeffs = pywt.wavedec(channel_data, 'db4', level=level)
+        return coeffs
+    else:
+        cA, cD = pywt.dwt(channel_data, 'db4')
+        return cA, cD
+
+
+def continuous_wavelet_transform(channel_data, scales, wavename, sampling_period):
+    cwtmatr, freqs = pywt.cwt(channel_data, scales, wavelet=wavename, sampling_period=sampling_period)
+
+    return cwtmatr, freqs
--- dispatch/dispatch_hub.py
+++ dispatch/dispatch_hub.py
@@ -5,6 +5,7 @@ from data_preprocessing.filters import data_filtering, multi_dataset_filtering
 from data_preprocessing.init_dataset import init, get_dataset_paths, create_dataset
 from data_preprocessing.data_distribution import data_preparation, online_data_labeling, normalization
 from data_preprocessing.recurrence_quantification_analysis import recurrence_quantification
+from data_preprocessing.wavelet_transformation import wavelet_transformation
 from data_training.EEGModels.training import EEGModels_training_hub
 from data_training.SVM.training import svm_training_hub
 from data_visualization.mne_visualization import visualize_mne
@@ -28,14 +29,14 @@ def dispatch(subject_id, config):
     of movement and end of movement. 
     """
     multi_dataset_onset_detection(training_data, config)
-    multi_dataset_onset_detection(online_data, config, is_online=True)
+    multi_dataset_onset_detection(online_data, config, is_online=False)
 
     """
     Modules to filter the data, functions can take variety of default frequency bands annotated in the 
     json_config/default.json file. Method include possibility of handling multiple datasets at once. 
     """
 
-    multi_dataset_filtering(config.ALPHA_BAND, config, training_data)
+    multi_dataset_filtering(config.BETA_BAND, config, training_data)
     multi_dataset_filtering(config.BASELINE, config, online_data)
     data_filtering(config.BASELINE, config, dwell_data)
 
@@ -45,8 +46,12 @@ def dispatch(subject_id, config):
     # downsample(training_data, config)
     # visualize_mne(training_data, config)
     # visualize_brain_activity(training_data, config)
-    recurrence_quantification(training_data, config)
 
+    """
+    Feature extraction. 
+    """
+    # recurrence_quantification(training_data, config, plot=True)
+    wavelet_transformation(training_data[0], config, wavelet_type='discrete', plot=True)
 
     """
     Prepare data for the models by combining the training datasets into a single vector. Each sample is cut
