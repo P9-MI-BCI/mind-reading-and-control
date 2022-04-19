aa00477a3b1c760c0e0419c1fc7fffcf653a64d9
TheKaergaard <31920988+TheKaergaard@users.noreply.github.com>
2022-04-19 16:21:28 +0200

    rsd test

--- modules/session_analysis.py
+++ modules/session_analysis.py
@@ -27,14 +27,11 @@ def run(config):
 
 def session_analysis_hub(training_data, online_data, dwell_data, config, subject_id, extend_data: bool = True,
                          save_res: bool = False):
-    # Mean, var, and rsd over each channel of all sessions.
-    mean, var, rsd = session_analysis(training_data, online_data, dwell_data, extend_data=extend_data)
+    # Mean and RSD over each channel of all sessions.
+    mean, rsd = session_analysis(training_data, online_data, dwell_data, extend_data=extend_data)
 
-    # std, rsd, and median (of the mean) of each channel over all sessions
+    # std, RSD, and median (of the mean) of each channel over all sessions
     std_mean, rsd_mean, median_mean = calc_std_rsd_median(mean)
-
-    # std, rsd, and median (of the var) of each channel over all sessions
-    std_var, rsd_var, median_var = calc_std_rsd_median(var)
 
     # Plotting and saving results
     boxplot(rsd, config, subject_id, 'rsd', show_points=True, extend_data=extend_data, save_fig=save_res)
@@ -61,27 +58,22 @@ def session_analysis(training_data: [Dataset], online_data: [Dataset], dwell_dat
         training_data.append(dwell_data)
 
     subject_mean = []
-    subject_var = []
     subject_rsd = []
     # Goes through the sessions of current subject (if extend_data = True online and dwell sessions are included)
     for session_data in training_data:
         session_data.data.drop(columns=['EMG'], inplace=True)
         session_mean = []
-        session_var = []
         session_rsd = []
         # Goes through each channel in each session
         for channel in session_data.data.columns:
             session_mean.append(session_data.data[channel].mean())
-            session_var.append(session_data.data[channel].var())
-
             rsd = relative_standard_deviation(session_data.data[channel].to_list())
             session_rsd.append(rsd)
 
         subject_mean.append(session_mean)
-        subject_var.append(session_var)
         subject_rsd.append(session_rsd)
 
-    return subject_mean, subject_var, subject_rsd
+    return subject_mean, subject_rsd
 
 
 #  Calculates the std, rsd, and median of the mean of each channel over ALL sessions.
@@ -101,7 +93,7 @@ def calc_std_rsd_median(session_res):
 def relative_standard_deviation(single_channel_data) -> float:
     std = np.std(single_channel_data)
     mean = np.mean(single_channel_data)
-    rsd = std / abs(mean)
+    rsd = 100 * std / mean
 
     return rsd
 
@@ -190,8 +182,10 @@ def barchart(session_results, config, subject_id: int, measure: str, save_fig: b
 
 
 def write_results_to_csv(result: [float], subject_id: int, config):
+    channels = config.EEG_CHANNELS.copy()
+    channels.append('EOG')
     path = os.path.join(OUTPUT_PATH, 'session_analysis', 'analysis_results.csv')
-    header = ['subject_id'] + config.EEG_CHANNELS
+    header = ['subject_id'] + channels
 
     try:
         with open(path, 'a', encoding='UTF8', newline='') as file:
